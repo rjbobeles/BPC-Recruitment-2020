c67fd4d38c11ce60f2a5fa3f315ff768293c7737
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-09-23 17:29:21 +0800

    Added form component

---
+++ src/components/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Position, Course } from '@/types'
+
+@Component
+export default class SubmitForm extends Vue {
+  @Prop() private position!: Array<Position>
+
+  @Prop() private course!: Array<Course>
+}
+</script>
