06f9e8d471db37f16f2fc22b9da7d711a46fafe1
Gel Gamoras <gelgamoras@gmail.com>
2020-09-26 20:22:35 +0800

    installed xmodal and vuetilt

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import lax from 'lax.js'
+import xmodal from 'xmodal-vue'
+import VueTilt from 'vue-tilt.js'
 import App from './App.vue'
 import router from './router'
 
@@ -12,6 +14,8 @@ require('dotenv').config()
 Vue.config.productionTip = false
 Vue.use(Vuelidate)
 Vue.use(lax)
+Vue.use(xmodal)
+Vue.use(VueTilt)
 Vue.prototype.$lax = lax
 
 new Vue({
