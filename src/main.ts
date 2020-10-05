2591c96ef2a6c2fb248583ed39de0e634ca73ffc
Gel Gamoras <gelgamoras@gmail.com>
2020-10-05 22:43:29 +0800

    installed rellax and smoothscroll

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import xmodal from 'xmodal-vue'
 import VueTilt from 'vue-tilt.js'
+import VueRellax from 'vue-rellax'
+import VueSmoothScroll from 'vue2-smooth-scroll'
 import App from './App.vue'
 import router from './router'
 
@@ -14,6 +16,10 @@ Vue.config.productionTip = false
 Vue.use(Vuelidate)
 Vue.use(xmodal)
 Vue.use(VueTilt)
+Vue.use(VueRellax)
+Vue.use(VueSmoothScroll, {
+  duration: 1000,
+})
 
 new Vue({
   router,
